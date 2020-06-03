ee9f953e58b0bf0987e3dc251ea96c512ace39d3
Wargen Guittap <wargenng@gmail.com>
2020-06-02 20:15:53 -0700

    view and create posts

--- rh-functions/functions/index.js
+++ rh-functions/functions/index.js
@@ -1,8 +1,61 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const config = require("./utility/config");
+const serviceAccount = require("./utility/service");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId,
+});
+
+const express = require("express");
+const app = express();
+
+app.get("/posts", (req, res) => {
+  admin
+    .firestore()
+    .collection("posts")
+    .get()
+    .then((data) => {
+      let posts = [];
+      data.forEach((doc) => {
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(posts);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post("/post", (req, res) => {
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then((doc) => {
+      return res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
